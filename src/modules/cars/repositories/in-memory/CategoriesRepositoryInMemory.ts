b5299fa6068f4a91c4dc648e2cb5303b9a9ee3e1
gregory6z <gregoryrag@gmail.com>
2022-05-24 17:34:22 +0200

    createUser Test

---
+++ src/modules/cars/repositories/in-memory/CategoriesRepositoryInMemory.ts
@@ -0,0 +1,25 @@
+import { Category } from "../../entities/Category";
+import {
+  ICategoriesRepository,
+  ICreteCategoryDTO,
+} from "../ICategoriesRepository";
+
+export class CategoriesRepositoryInMemory implements ICategoriesRepository {
+  categories: Category[] = [];
+
+  async findByName(name: string): Promise<Category> {
+    const category = this.categories.find((category) => category.name === name);
+
+    return category;
+  }
+  async list(): Promise<Category[]> {
+    const all = this.categories;
+    return all;
+  }
+  async create({ name, description }: ICreteCategoryDTO): Promise<void> {
+    const category = new Category();
+
+    Object.assign(category, { name, description });
+    this.categories.push(category);
+  }
+}
