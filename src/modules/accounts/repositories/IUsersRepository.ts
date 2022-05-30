7c3c29da02b0d6ed2f5eb80c2f4fc45a832b16cc
gregory6z <gregoryrag@gmail.com>
2022-05-30 18:28:12 +0200

    create migration cars

--- src/modules/accounts/repositories/IUsersRepository.ts
+++ src/modules/accounts/repositories/IUsersRepository.ts
@@ -1,5 +1,5 @@
 import { ICreateUserDTO } from "../dtos/ICreateUserDTO";
-import { User } from "../../../shared/infra/typeorm/entities/User";
+import { User } from "../infra/typeorm/entities/User";
 
 export interface IUsersRepository {
   create(data: ICreateUserDTO): Promise<void>;
