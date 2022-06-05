65a0e7fea3d7976f56dda71633366e808d8791e5
gregory6z <gregoryrag@gmail.com>
2022-06-05 12:44:45 +0200

    Devolution Rental UseCase

--- src/modules/rentals/infra/typeorm/repositories/RentalsRepository.ts
+++ src/modules/rentals/infra/typeorm/repositories/RentalsRepository.ts
@@ -36,4 +36,8 @@ export class RentalsRepository implements IRentalsRepository {
 
     return rental;
   }
+  async findById(id: string): Promise<Rental> {
+    const rental = await this.repository.findOne(id);
+    return rental;
+  }
 }
--- src/modules/rentals/repositories/IRentalsRepository.ts
+++ src/modules/rentals/repositories/IRentalsRepository.ts
@@ -4,6 +4,7 @@ import { Rental } from "../infra/typeorm/entities/Rental";
 export interface IRentalsRepository {
   findOpenRentalByCar(car_id: string): Promise<Rental>;
   findOpenRentalByUser(user_id: string): Promise<Rental>;
+  findById(id: string): Promise<Rental>;
   create({
     car_id,
     expected_return_date,
