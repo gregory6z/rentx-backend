0685d5eada533f062def3eacd7e2b68c09177b1f
gregory6z <gregoryrag@gmail.com>
2022-06-07 15:32:21 +0200

    create sendForgotPassword use case

--- src/shared/container/providers/DateProvider/implementations/DayjsDateProvider.ts
+++ src/shared/container/providers/DateProvider/implementations/DayjsDateProvider.ts
@@ -28,4 +28,7 @@ export class DayjsDateProvider implements IDateProvider {
   addDays(days: number): Date {
     return dayjs().add(days, "days").toDate();
   }
+  addHours(hours: number): Date {
+    return dayjs().add(hours, "hour").toDate();
+  }
 }
