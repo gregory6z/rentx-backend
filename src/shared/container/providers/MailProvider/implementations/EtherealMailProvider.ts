53731f533de01e28189d776786e1600edde2e29e
gregory6z <gregoryrag@gmail.com>
2022-06-11 20:51:04 +0200

    template engine

--- src/shared/container/providers/MailProvider/implementations/EtherealMailProvider.ts
+++ src/shared/container/providers/MailProvider/implementations/EtherealMailProvider.ts
@@ -1,3 +1,5 @@
+import fs from "fs";
+import handlebars from "handlebars";
 import nodemailer, { Transporter } from "nodemailer";
 import { injectable } from "tsyringe";
 
@@ -27,16 +29,25 @@ export class EtherealMailProvider implements IMailProvider {
       });
   }
 
-  async sendMail(to: string, subject: string, body: string): Promise<void> {
+  async sendMail(
+    to: string,
+    subject: string,
+    variables: any,
+    path: string,
+  ): Promise<void> {
+    const templateFileContent = fs.readFileSync(path).toString("utf-8");
+
+    const templateParse = handlebars.compile(templateFileContent);
+
+    const templateHTML = templateParse(variables);
+
     const message = await this.client.sendMail({
       to,
       subject,
       from: "RentX <noreplay@rentx.com.br>",
-      text: body,
-      html: body,
+      html: templateHTML,
     });
     console.log("Message sent: %s", message.messageId);
-    // Preview only available when sending through an Ethereal account
     console.log("Preview URL: %s", nodemailer.getTestMessageUrl(message));
   }
 }
